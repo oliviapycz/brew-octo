6b5d818da5455cffa55bfabe6b3cb69d6ce80ab0
Oliv <oliv@octo.com>
2020-01-24 14:34:24 +0100

    add useEffect to fetched beers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [beerList, setBeerList] = useState([])
+  
+  useEffect(() => {
+    axios.get("https://api.punkapi.com/v2/beers")
+    .then(response => {
+      setBeerList(response.data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ul>
+          {beerList.map(item => (
+            <li key={item.id}>
+              <article>
+                <img src={item.image_url} ></img>
+                <h1>{item.name}</h1>
+                <p>{item.description}</p>
+              </article>
+            </li>
+          ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
